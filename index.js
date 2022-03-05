c13dae531fa76c6505f570d54eeebf38a8d1dcd1
Andreas Georgiou <AGeorgiou4@uclan.ac.uk>
2022-03-05 06:24:02 +0200

    Starting script

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const loans = require('./calls/loans.js');
 const bibliographies = require('./calls/bibliographies.js');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -21,5 +21,3 @@ app.use('/', bibliographies);
 app.listen(port, () => {
     console.log(`REST API app listening at http://localhost:${port}`)
 });
-
-//test
