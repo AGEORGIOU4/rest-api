f9983ed51e7bf3bcb83c4c35fb86ccc5a724398e
Andreas Georgiou <AGeorgiou4@uclan.ac.uk>
2022-03-05 07:08:20 +0200

    package-json

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 
-// const books = require('./calls/books.js');
-// const students = require('./calls/students.js');
-// const modules = require('./calls/modules.js');
-// const loans = require('./calls/loans.js');
-// const bibliographies = require('./calls/bibliographies.js');
+const books = require('./calls/books.js');
+const students = require('./calls/students.js');
+const modules = require('./calls/modules.js');
+const loans = require('./calls/loans.js');
+const bibliographies = require('./calls/bibliographies.js');
 
 const app = express();
 let port = process.env.PORT || 3000;
@@ -15,11 +15,11 @@ app.get("/", (req, res) => {
 app.use(express.json());
 
 // ===== Calls ===== //
-// app.use('/', books);
-// app.use('/', students);
-// app.use('/', modules);
-// app.use('/', loans);
-// app.use('/', bibliographies);
+app.use('/', books);
+app.use('/', students);
+app.use('/', modules);
+app.use('/', loans);
+app.use('/', bibliographies);
 
 app.listen(port, () => {
     console.log(`REST API app listening at http://localhost:${port}`)
